e175331ab58b757515e4556da8318c85988e003d
Salvo-Raia <salvo.raia@outlook.it>
2026-02-04 19:03:57 +0100

    Header structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import Header from "./components/header";
 import Main from "./components/main";
 import Footer from "./components/footer";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,23 @@
 export default function Header () {
-    return <header>Header</header>
+    return <header className="container text-center py-3">    
+             <div className="header-wrapper d-flex justify-content-between align-items-center">
+                <div className="logo">
+                    <img src="/dc-logo.png" title="DC Comics logo" alt="DC Logo" />
+                </div>
+                <div className="header-navbar">
+                    <ul className="d-flex gap-3 p-0 m-0">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </div>
+             </div>
+           </header>
 }
