2208e714011a6bf43eaa19190caf11b1e4472de0
Salvo-Raia <salvo.raia@outlook.it>
2026-02-04 18:34:38 +0100

    Init repository - Basic structure & Scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
+
+export default function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    </>
+  )
+}
+
