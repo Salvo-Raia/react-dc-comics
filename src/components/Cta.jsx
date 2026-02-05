59963787c731f11ea39b41c2eda110d08a801aea
Salvo-Raia <salvo.raia@outlook.it>
2026-02-05 17:41:54 +0100

    Add iteration for CTA links

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,11 +1,43 @@
 export default function Cta () {
+
+const ctaLinks = [
+  {
+    url: "#",
+    link: "DIGITAL COMICS",
+    img: "./src/assets/img/buy-comics-digital-comics.png",
+    imgDescription: "Buy Digital Comics icon"
+  },
+    {
+    url: "#",
+    link: "DIGITAL COMICS",
+    img: "./src/assets/img/buy-comics-digital-comics.png",
+    imgDescription: "Buy Digital Comics icon"
+  },
+    {
+    url: "#",
+    link: "DIGITAL COMICS",
+    img: "./src/assets/img/buy-comics-digital-comics.png",
+    imgDescription: "Buy Digital Comics icon"
+  },
+    {
+    url: "#",
+    link: "DIGITAL COMICS",
+    img: "./src/assets/img/buy-comics-digital-comics.png",
+    imgDescription: "Buy Digital Comics icon"
+  },
+    {
+    url: "#",
+    link: "DIGITAL COMICS",
+    img: "./src/assets/img/buy-comics-digital-comics.png",
+    imgDescription: "Buy Digital Comics icon"
+  }
+]; 
+
     return <div className="container py-5">
               <ul className="cta-navbar d-flex justify-content-between p-0 m-0">
-                <li><a href="#"><img src="./src/assets/img/buy-comics-digital-comics.png" title="Buy Digital Comics icon" alt="Buy Digital Comics icon" /><span className="ps-3">DIGITAL COMICS</span></a></li>
-                <li><a href="#"><img src="./src/assets/img/buy-comics-merchandise.png" title="Buy Comics Merchandise icon" alt="Buy Comics Merchandise icon" /><span className="ps-3">DC MERCHANDISE</span></a></li>
-                <li><a href="#"><img src="./src/assets/img/buy-comics-subscriptions.png" title="Subscription icon" alt="Subscription icon" /><span className="ps-3">SUBSCRIPTION</span></a></li>
-                <li><a href="#"><img src="./src/assets/img/buy-comics-shop-locator.png" title="Comic Shop Locator icon" alt="Comic Shop Locator icon" /><span className="ps-3">COMIC SHOP LOCATOR</span></a></li>
-                <li><a href="#"><img src="./src/assets/img/buy-dc-power-visa.svg" title="DC Power Visa Icon" alt="DC Power Visa Icon" /><span className="ps-3">DC POWER VISA</span></a></li>
-              </ul>
+                {ctaLinks.map((ctaLinks,index) => 
+                <li key={index}><a href={ctaLinks.url}><img src={ctaLinks.img} alt={ctaLinks.imgDescription} /><span className="ps-3">{ctaLinks.link}</span></a></li>
+              )}
+                </ul>
            </div>
 }
