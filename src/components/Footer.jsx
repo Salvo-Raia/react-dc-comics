e8331fc1f4b63e327a63e2a57a67a0ec3f01c0c5
Salvo-Raia <salvo.raia@outlook.it>
2026-02-05 17:31:40 +0100

    Add iteration for footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,55 @@
 import Cta from "./Cta"
 
 export default function Footer () {
+
+    const footerNav = [
+  {
+    title: "DC COMICS",
+    links: [
+      { url: "#", link: "Characters" },
+      { url: "#", link: "Comics" },
+      { url: "#", link: "Movies" },
+      { url: "#", link: "TV" },
+      { url: "#", link: "Games" },
+      { url: "#", link: "Videos" },
+      { url: "#", link: "News" }
+    ]
+  },
+  {
+      title: "DC",
+      links: [
+          { url: "#", link: "Terms of Use" },
+          { url: "#", link: "Privacy Policy(New)" },
+          { url: "#", link: "Ad Choices" },
+          { url: "#", link: "Advertising" },
+          { url: "#", link: "Jobs" },
+          { url: "#", link: "Subscriptions" },
+          { url: "#", link: "Talent Workshops" },
+          { url: "#", link: "CPSC Certificates" },
+          { url: "#", link: "Ratings" },
+          { url: "#", link: "Shop Help" },
+          { url: "#", link: "Contact Us" }
+        ]
+    },
+    {
+      title: "SITES",
+      links: [
+        { url: "#", link: "DC" },
+        { url: "#", link: "MAD Magazine" },
+        { url: "#", link: "DC Kids" },
+        { url: "#", link: "DC Universe" },
+        { url: "#", link: "DC Power Visa" }
+      ]
+    },
+    {
+      title: "SHOP",
+      links: [
+        { url: "#", link: "Shop DC" },
+        { url: "#", link: "Shop DC Collectibles" }
+      ]
+    }
+];
+
     return <footer>
             <div className="call-to-action">
             <Cta>
@@ -9,48 +58,17 @@ export default function Footer () {
             <div className="footer-navbar">
                 <div className="container d-flex justify-content-between py-5">
                     <div className="footer-navbar-menù d-flex gap-5 text-white">
-                        <div>
-                        <h4 className="mb-3">DC COMICS</h4>
-                        <ul className="p-0">
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
-                        </ul>
-                        <h4 className="mb-3">SHOP</h4>
-                        <ul className="p-0">
-                            <li>Shop DC</li>
-                            <li>Shop DC Collectibles</li>
-                        </ul>
-                        </div>
-                        <div>
-                        <h4 className="mb-3">DC</h4>
-                        <ul className="p-0">
-                            <li>Terms of Use</li>
-                            <li>Privacy Policy(New)</li>
-                            <li>Ad Choices</li>
-                            <li>Advertising</li>
-                            <li>Jobs</li>
-                            <li>Subscriptions</li>
-                            <li>Talent Workshops</li>
-                            <li>CPSC Certificates</li>
-                            <li>Ratings</li>
-                            <li>Shop Help</li>
-                            <li>Contact Us</li>
-                        </ul>
-                        </div>
-                        <div>
-                        <h4 className="mb-3">SITES</h4>
-                        <ul className="p-0">
-                            <li>DC</li>
-                            <li>MAD Magazine</li>
-                            <li>DC Kids</li>
-                            <li>DC Universe</li>
-                            <li>DC Power Visa</li>
-                        </ul>
+                        <div className="row row-cols-3">
+                            {footerNav.map((footerNav, index) => 
+                                <div key={index} className="col text">
+                                    <h4>{footerNav.title}</h4>
+                                    <ul className="p-0">
+                                        {footerNav.links.map((singleLink, index) => 
+                                        <li key={index}><a href={singleLink.url}>{singleLink.link}</a></li>
+                                        )}
+                                    </ul>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <div className="big-footer-logo">
