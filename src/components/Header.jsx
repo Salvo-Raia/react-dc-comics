b318519863b13040614389098b42cd1a9c0c8702
Salvo-Raia <salvo.raia@outlook.it>
2026-02-05 16:08:46 +0100

    Add iteration for header Nav + Active class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,65 @@
 export default function Header () {
+
+    const headerNav = [
+        {
+            url: "#",
+            link: "CHARACTERS",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "COMICS",
+            isActive: true
+        },
+         {
+            url: "#",
+            link: "MOVIES",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "TV",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "GAMES",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "COLLECTIBLES",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "VIDEOS",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "FANS",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "NEWS",
+            isActive: false
+        },
+         {
+            url: "#",
+            link: "SHOP",
+            isActive: false
+        },
+    ];
+
     return <header className="text-center ">    
              <div className="header-wrapper container d-flex justify-content-between align-items-center">
                 <div className="logo">
                     <img src="/dc-logo.png" title="DC Comics logo" alt="DC Logo" />
                 </div>
                     <ul className="d-flex gap-4 p-0 m-0">
-                        <li>CHARACTERS</li>
-                        <li className="active">COMICS</li>
-                        <li>MOVIES</li>
-                        <li>TV</li>
-                        <li>GAMES</li>
-                        <li>COLLECTIBLES</li>
-                        <li>VIDEOS</li>
-                        <li>FANS</li>
-                        <li>NEWS</li>
-                        <li>SHOP</li>
+                        {headerNav.map((navItem, index) => (<li key={index} className={(navItem.isActive) ? "active" : ""} ><a href={navItem.url}></a>{navItem.link}</li>))}
                     </ul>
              </div>
            </header>
