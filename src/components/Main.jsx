b818bba645a2e58160e7b1aff0d03f828f2cecc5
Salvo-Raia <salvo.raia@outlook.it>
2026-02-05 16:54:57 +0100

    Add iteration for main contents

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,20 @@
+import comics from "./comics";
+
 export default function Main () {
+
     return <main className=" bg-dark text-white">
         <div className="container text-center py-5">
-        <h2 className="m-0"> -- Main content goes here -- </h2>
+            <div className="row row-cols-6">
+                {comics.map((comicIssue) => 
+                    <div key={comicIssue.id} className="col">
+                        <div className="comic-card">
+                            <img className="img-fluid" src={comicIssue.thumb} title={comicIssue.series} alt={comicIssue.series} />
+                            <h3 className="h6 my-3">{comicIssue.series}</h3>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <button className="btn btn-primary loadmorebtn rounded-0 px-5">Load More</button>
         </div>
     </main>
 }
